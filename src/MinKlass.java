bfaf30304030a0282a6e7219d104d2c953c986c9
HugoCortesLopez <hugo.corteslopez@yh.nackademin.se>
2020-09-29 15:17:36 +0200

    Initial commit

---
+++ src/MinKlass.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Hugo Cortes Lopez
+ * Date:    2020-09-29
+ * Time:    15:11
+ * Project: testProj
+ */
+public class MinKlass {
+    public static void main(String[] args) {
+        System.out.println("hej");
+    }
+}
